1e99476799d6cf9e7ddbb34aed00d5b40ef4ed64
heliu <heliu@tcd.ie>
2019-09-27 20:00:18 +0100

    Added a isInTheTree function and some Junit Tests for it

--- src/LCA.java
+++ src/LCA.java
@@ -1,10 +1,26 @@
 public class LCA {
 	//The root node of the binary tree.	
+	static boolean inTheTree;
 	
 	
 	public static Node searchLCA(Node root,int value1,int value2)
 	{
-		return searchLCAP(root,value1,value2);
+		boolean check;
+		//check if the value1 is in the tree
+		isInTheTree(root,value1);
+		check = inTheTree;
+		//check if the value2 is in the tree
+		isInTheTree(root,value2);
+		//if both value are in the tree then start search
+		if(check && inTheTree)
+		{
+			return searchLCAP(root,value1,value2);
+		}
+		//else just return a null
+		else
+		{
+			return null;
+		}
 	}
 	
 	private static Node searchLCAP(Node theNode,int value1,int value2)
@@ -24,8 +40,8 @@ public class LCA {
 		}
 		
 		//Search
-		Node leftResult = searchLCA(theNode.left,value1,value2);
-		Node rightResult = searchLCA(theNode.right,value1,value2);
+		Node leftResult = searchLCAP(theNode.left,value1,value2);
+		Node rightResult = searchLCAP(theNode.right,value1,value2);
 		
 		//If one value is in a side, and the other value is in the other side, then the current node is the LCA.
 		if(leftResult != null && rightResult != null)
@@ -35,16 +51,57 @@ public class LCA {
 		//If both values are in the right side, then set the right child node as the new root node.
 		else if(leftResult == null)
 		{
-			return searchLCA(rightResult,value1,value2);
+			return searchLCAP(rightResult,value1,value2);
 		}
 		//If both values are in the left side, then set the left child node as the new root node.
 		else if(rightResult == null)
 		{
-			return searchLCA(leftResult,value1,value2);
+			return searchLCAP(leftResult,value1,value2);
 		}
 		else
 		{
 			return null;
+		}
+	}
+	
+	//a function to check if a given value is inside the given tree, after run this function, check isTheTree to see the result.
+	public static void isInTheTree(Node root,int value)
+	{
+		//use a global variable to check the final result
+		inTheTree = false;
+		//if the root is null just return null
+		if(root!=null)
+		{
+			isInTheTreeP(root,value);
+		}
+		
+	}
+	
+	private static void isInTheTreeP(Node theNode,int value)
+	{
+		//if found the node, then set the flag to true
+		if(theNode.data == value)
+		{
+			inTheTree = true;
+		}
+		//else go to the child nodes to continue searching
+		else
+		{
+			if(theNode.left !=null && theNode.right !=null)
+			{
+				isInTheTreeP(theNode.left,value);
+				isInTheTreeP(theNode.right,value);
+			}
+			if(theNode.left == null && theNode.right != null)
+			{
+				isInTheTreeP(theNode.right,value);
+			}
+			if(theNode.right == null && theNode.left != null)
+			{
+				isInTheTreeP(theNode.left,value);
+			}
+			//if a node has no child node, return
+				return;
 		}
 	}
 	
--- src/LCAtest.java
+++ src/LCAtest.java
@@ -4,6 +4,41 @@ import org.junit.Test;
 
 
 public class LCAtest {
+	
+	
+	@Test
+	public void test_isInTheTree()
+	{
+		/**
+		 *       1
+		 *      / \
+		 *     2   3
+		 *    / \
+		 *   4   5
+		 */
+		
+		// create a root node with value 1
+		Node root = new Node(1,null,null);
+		root.left = new Node(2,null,null);         
+		root.right = new Node(3,null,null);       
+		root.left.left = new Node(4,null,null);                
+		root.left.right = new Node(5,null,null);
+		
+		LCA.isInTheTree(root, 2);
+		assertEquals("Checking for root value in the tree.",true,LCA.inTheTree);
+		
+		LCA.isInTheTree(root, 4);
+		assertEquals("Checking for leaf value in the tree.",true,LCA.inTheTree);
+		
+		LCA.isInTheTree(root, 5);
+		assertEquals("Checking for leaf value in the tree.",true,LCA.inTheTree);
+		
+		LCA.isInTheTree(root, 6);
+		assertEquals("Checking for non existing value in the tree.",false,LCA.inTheTree);
+		
+		
+		
+	}
 
 	@Test
 	public void testLCA() {
@@ -23,11 +58,13 @@ public class LCAtest {
 		root.left.left = new Node(4,null,null);                
 		root.left.right = new Node(5,null,null);
 		
+
 		assertEquals("Checking Node 2 and Node 4, the LSA should be 2",2,LCA.searchLCA(root,2,4).data);
 		assertEquals("Checking Node 4 and Node 5, the LSA should be 2",2,LCA.searchLCA(root,4,5).data);
 		assertEquals("Checking Node 4 and Node 3, the LSA should be 1",1,LCA.searchLCA(root,4,3).data);
 		
-		//assertEquals("Checking Node 6 and Node 3, the LSA should be null",null,LCA.searchLCA(root,6,3).data);
+		assertEquals("Checking Node 6 and Node 3, the LSA should be null",null,LCA.searchLCA(root,6,3));
+		assertEquals("Checking Node 7 and Node 8, the LSA should be null",null,LCA.searchLCA(root,7,8));
 		
 		
 		
